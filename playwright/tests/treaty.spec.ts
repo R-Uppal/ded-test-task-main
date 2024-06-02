28322bd398b3bb4dd5bff5026127d4848b7fafad
R-Uppal <ruppal667@gmail.com>
2024-06-02 22:30:03 +0100

    Adding element visibility checks and organising locators into variables to improve readability

--- playwright/tests/treaty.spec.ts
+++ playwright/tests/treaty.spec.ts
@@ -6,15 +6,20 @@ test.describe('treaty', () => {
     await page.goto('/');
   
     const anchorElement = page.locator('a[href="/how-we-do-it"]');
+    await expect(anchorElement).toBeVisible();
     await expect(anchorElement).toHaveText('how we do it');
     await anchorElement.click();
   
     
     await expect(page).toHaveURL('/how-we-do-it');
-    await page.locator('h3', { hasText: 'treaty' }).click();
+    const treatyHeading = page.locator('h3', { hasText: 'treaty' });
+    await expect(treatyHeading).toBeVisible();
+    await treatyHeading.click();
+
     await expect(page).toHaveURL('/how-we-do-it/treaty');
   
     const specificH2Element = page.locator('h2', { hasText: 'The Demilitarise Education Treaty' });
+    await expect(specificH2Element).toBeVisible();
     await expect(specificH2Element).toHaveText('The Demilitarise Education Treaty');
   });
 });
