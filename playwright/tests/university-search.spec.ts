d9161914f815d1b78433c97a59e79a41a360fce7
R-Uppal <ruppal667@gmail.com>
2024-06-02 22:42:39 +0100

    update university search tests

--- playwright/tests/university-search.spec.ts
+++ playwright/tests/university-search.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('navigation', () => {
+test.describe('university_search_home', () => {
   test('University search on home page yields expected results.', async ({ page }) => {
     const searchText = "bri";
     const targetName = "University of the West of England, Bristol";
-    const targetPath = "/data/university/2";
+    const targetPath = "/data/university/20";
     const expectedLinkText = [
       "policies",
       "financial partnerships",
@@ -17,15 +17,21 @@ test.describe('navigation', () => {
     await page.setViewportSize({ width: 1600, height: 1200 });
     await page.goto('/');
   
-    await page.locator("input.ded-input-search-university").fill(searchText);
+    const searchInput = page.locator("input.ded-input-search-university");
+    await searchInput.fill(searchText);
   
     const container = page.locator("div.ded-university-search-result-container");
-    const searchResultButton = container.locator('> button', { hasText: targetName });
+    const searchResultButton = container.locator('button', { hasText: targetName });
     await searchResultButton.click();
   
-    await expect(page).toHaveURL(new RegExp(targetPath)); // Allows the URL assertion to check for a partial match, similar to how Cypress's cy.url().should('include', targetPath); works.
+    await expect(page).toHaveURL(targetPath); 
     const h2Element =  page.locator('h2', { hasText: targetName })
+    await expect(h2Element).toBeVisible();
     await expect(h2Element).toHaveText(targetName);
+
+    const breadcrumbElement = page.locator(".ded-breadcrumbs");
+    await expect(breadcrumbElement).toBeVisible();
+    await expect(breadcrumbElement).toContainText(targetName);
   
     expectedLinkText.forEach((linkText) => {
       page.locator('a', { hasText: linkText })
